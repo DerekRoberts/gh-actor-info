690c959a3a7fcc7deba4f626a49726e30e83c7f2
Derek Roberts <derek.roberts@gmail.com>
2021-08-30 11:29:27 -0700

    fix: clean out output for jq

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 const ghActor: string = process.argv[2] || '';
 if (!ghActor) {
   console.error('\nPlease provide a username');
-  console.error(' e.g.: ./dist/index.js --username=octocat');
+  console.error(' e.g.: ./dist/index.js octocat');
   process.exit(1);
 }
 
@@ -27,7 +27,7 @@ export async function getFromPublished(actor: string): Promise<User> {
   await axios
     .get(`https://api.github.com/users/${actor}`)
     .then((response) => {
-      user.email = response.data.email;
+      user.email = response.data.email || '';
       user.name = response.data.name;
     })
     .catch((error) => {
@@ -46,7 +46,7 @@ export async function getFromCommits(actor: string): Promise<User> {
         user.email = response.data[0].payload.commits[0].author.email;
         user.name = response.data[0].payload.commits[0].author.name;
       } else {
-        console.log(`No commits found for user ${actor}`);
+        return getFromPublished(actor);
       }
     })
     .catch((error) => {
@@ -56,5 +56,4 @@ export async function getFromCommits(actor: string): Promise<User> {
   return user;
 }
 
-// console.log(await getFromPublished(ghActor));
-console.log(await getFromCommits(ghActor));
+console.log(JSON.stringify(await getFromCommits(ghActor)));
